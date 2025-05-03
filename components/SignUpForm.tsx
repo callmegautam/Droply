f8da650f67f08b5687268e91b2b8a3bae111f035
Gautam Suthar <iamgautamsuthar@outlook.com>
2025-05-03 19:49:25 +0530

    Enhance SignUpForm with form handling using react-hook-form and Zod validation

--- components/SignUpForm.tsx
+++ components/SignUpForm.tsx
@@ -5,16 +5,25 @@ import { useSignUp } from '@clerk/nextjs';
 import { z } from 'zod';
 import { signUpSchema } from '@/schemas/signUpSchema';
 import { useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 export default function SignUpForm() {
     const [verifying, setVerifying] = useState(false);
     const { signUp, isLoaded, setActive } = useSignUp();
 
+    // * SignUp Form
     const {
         register,
         handleSubmit,
         formState: { errors },
-    } = useForm<z.infer<typeof signUpSchema>>({});
+    } = useForm<z.infer<typeof signUpSchema>>({
+        resolver: zodResolver(signUpSchema),
+        defaultValues: {
+            email: '',
+            password: '',
+            passwordConfirmation: '',
+        },
+    });
     const onSubmit = async () => {};
     const handleVerificationSubmit = async () => {};
 
